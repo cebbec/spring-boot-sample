ef0f06d147106609408e9b5c11952c970e7aba3a
kurofune <miesque_bcm2@yahoo.co.jp>
2016-01-13 02:27:56 +0900

    make sample project.

---
+++ src/main/java/sample/controller/IndexController.java
@@ -0,0 +1,18 @@
+package sample.controller;
+
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("msg", "Hello, world!");
+		return "index";
+	}
+}
